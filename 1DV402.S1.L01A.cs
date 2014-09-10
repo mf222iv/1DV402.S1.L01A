5887462490ed3b2a4ea5b813073d44db34e0b0d1
Michael Forsberg <mf222iv@student.lnu.se>
2014-09-10 16:47:59 +0200

    Try Catch

--- 1DV402.S1.L01A.cs
+++ 1DV402.S1.L01A.cs
@@ -20,35 +20,63 @@ namespace ConsoleApplication2
             int returnFive;
             int returnOne;
 
-            Console.Write("Ange totalsumma:     ");
-            sum = double.Parse(Console.ReadLine());
-            Console.Write("Ange erhållet belopp:     ");
-            receivedAmount = int.Parse(Console.ReadLine());
-            toPay = (int) sum;
-            double Rounding = Math.Round(sum, 0) - sum;
-            returnAmount = receivedAmount - (int)sum;
-            returnFivehundred = returnAmount / 500;
-            returnHundred = (returnAmount - returnFivehundred * 500) / 100;
-            returnTwenty = (returnAmount - returnFivehundred * 500 - returnHundred * 100) / 20;
-            returnFive = (returnAmount - returnFivehundred * 500 - returnHundred * 100 - returnTwenty * 20) / 5;
-            returnOne = (returnAmount - returnFivehundred * 500 - returnHundred * 100 - returnTwenty * 20 - returnFive * 5) / 1;
+            try
+            {
+                Console.Write("Ange totalsumma:     ");
+                sum = double.Parse(Console.ReadLine());
+                if (sum < 1)
+                    {
+                        Console.WriteLine("Summan är för litet!");
+                    }
+                else
+                {
+                    Console.Write("Ange erhållet belopp:     ");
+                    receivedAmount = int.Parse(Console.ReadLine());
+                    if (receivedAmount < sum)
+                    {
+                        Console.WriteLine("Det mottagna beloppet är för litet!");
+                    }
+                else
+                {
+                    toPay = (int)sum;
+                    double Rounding = Math.Round(sum, 0) - sum;
+                    returnAmount = receivedAmount - (int)sum;
+                    returnFivehundred = returnAmount / 500;
+                    returnHundred = (returnAmount - returnFivehundred * 500) / 100;
+                    returnTwenty = (returnAmount - returnFivehundred * 500 - returnHundred * 100) / 20;
+                    returnFive = (returnAmount - returnFivehundred * 500 - returnHundred * 100 - returnTwenty * 20) / 5;
+                    returnOne = (returnAmount - returnFivehundred * 500 - returnHundred * 100 - returnTwenty * 20 - returnFive * 5) / 1;
 
 
-            Console.WriteLine("KVITTO");
-            Console.WriteLine("-------------------------------------------");
-            Console.WriteLine("Totalt           :               {0, 10:c}", sum);
-            Console.WriteLine("Öresavrundning   :               {0, 10:c}", Rounding);
-            Console.WriteLine("Att betala       :               {0, 10:c}", toPay);
-            Console.WriteLine("Kontant          :               {0, 10:c}", receivedAmount);
-            Console.WriteLine("Tillbaka         :               {0, 10:c}", returnAmount);
-            Console.WriteLine("-------------------------------------------");
+                    Console.WriteLine("KVITTO");
+                    Console.WriteLine("-------------------------------------------");
+                    Console.WriteLine("Totalt           :               {0, 10:c}", sum);
+                    Console.WriteLine("Öresavrundning   :               {0, 10:c}", Rounding);
+                    Console.WriteLine("Att betala       :               {0, 10:c}", toPay);
+                    Console.WriteLine("Kontant          :               {0, 10:c}", receivedAmount);
+                    Console.WriteLine("Tillbaka         :               {0, 10:c}", returnAmount);
+                    Console.WriteLine("-------------------------------------------");
 
-            Console.WriteLine("500-lappar       : {0}", returnFivehundred);
-            Console.WriteLine("100-lappar       : {0}", returnHundred);
-            Console.WriteLine("20-lappar        : {0}", returnTwenty);
-            Console.WriteLine("5-kronor         : {0}", returnFive);
-            Console.WriteLine("1-kronor         : {0}", returnOne);
+                    Console.WriteLine("500-lappar       : {0}", returnFivehundred);
+                    Console.WriteLine("100-lappar       : {0}", returnHundred);
+                    Console.WriteLine("20-lappar        : {0}", returnTwenty);
+                    Console.WriteLine("5-kronor         : {0}", returnFive);
+                    Console.WriteLine("1-kronor         : {0}", returnOne);
 
-        }
+               
+                }
+               
+              }
+          
+          
+           
+        
+ 
     }
+              catch
+            {
+                Console.WriteLine("Beloppet är felaktigt");
+            }
+}
+}
 }
